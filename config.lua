ed15e4b96d4512349ff72d0e92503b7fd9daaacc
XxKRONICxX <160303106+XxKRONICxX@users.noreply.github.com>
2025-01-09 03:35:08 -0600

    Update config.lua
    .

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 return {
 
     blip = {
-        enabled = true,
+        enabled = false,
         label = 'Laundromat',
         coords = vector3(1144.13, -985.58, 45.95),
         sprite = 77,
@@ -12,13 +12,13 @@ return {
     requireKeycard = true, -- use an item to enter the moneywash building.
         keycardItem = 'moneywash_key', 
 
-    itemRequired = true, -- use an item to use the machines
+    itemRequired = false, -- use an item to use the machines
         ticketItem = 'moneywash_token',
 
     washing = {
         duration = 10000, -- time to wash money (same no matter what amount)
         taxRate = 0.10, -- removes 10% of total money put in
-        markedMoney = 'markedbills', -- item put into the washer
+        markedMoney = 'black_money', -- item put into the washer
         cleanMoney = 'money',
     },
 
@@ -36,28 +36,28 @@ return {
         label = 'Enter Laundromat',
         coords = vector3(1142.34, -986.73, 46.11),
         icon = 'fa-solid fa-soap',
-        iconColor = '#76A9D2',
+        iconColor = '#FFFFFF',
     },
 
     targetExit = {
         label = 'Exit Laundromat',
         coords = vector4(1142.85, -986.73, 45.9, 277.81),
         icon = 'fa-solid fa-door-open',
-        iconColor = '#76A9D2',
+        iconColor = '#FFFFFF',
     },
 
     targetWash = {
         label = 'Wash Money',
         coords = vec3(1122.4954, -3193.2864, -40.3926),
         icon = 'fa-solid fa-money-bill',
-        iconColor = '#76A9D2',
+        iconColor = '#FFFFFF',
     },
 
     targetCount = {
         label = 'Dry & Count Money',
         coords = vector3(1116.36, -3194.58, -39.92),
         icon = 'fa-solid fa-money-bill',
-        iconColor = '#76A9D2',
+        iconColor = '#FFFFFF',
     },
     
 }
